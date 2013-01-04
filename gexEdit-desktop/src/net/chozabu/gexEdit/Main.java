bdc6bae1a29d337c74296d86c0212e31dc2575f9
Chozabu <chozabu@gmail.com>
2013-01-04 10:37:27 +0000

    not much

---
+++ gexEdit-desktop/src/net/chozabu/gexEdit/Main.java
@@ -0,0 +1,16 @@
+package net.chozabu.gexEdit;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "GexEdit";
+		cfg.useGL20 = false;
+		cfg.width = 640;
+		cfg.height = 480;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
